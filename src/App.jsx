9a3dfff7f6c16c62a3a2301078819dc4812aa328
MDeFaria <maria.defariasf@gmail.com>
2024-04-02 15:22:21 -0400

    add: routes for landing and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Home, Landing } from "./pages";
+import { Footer, Header } from "./components";
+
 function App() {
-  return <></>;
+  const { pathname } = useLocation();
+  return (
+    <>
+      <div>
+        {pathname !== "/" && <Header />}
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        {pathname !== "/" && <Footer />}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql",
@@ -12,7 +13,9 @@ const client = new ApolloClient({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>
 );
