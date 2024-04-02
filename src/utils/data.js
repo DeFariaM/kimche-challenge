f8412a4bdd85145e7366047205cf173f778f58f7
MDeFaria <maria.defariasf@gmail.com>
2024-04-02 18:37:47 -0400

    add: utils for query consultation and data use in selects

---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+export const speciesData = [
+  "Human",
+  "Alien",
+  "Humanoid",
+  "unknown",
+  "Poopybutthole",
+  "Mythological Creature",
+  "Animal",
+  "Robot",
+  "Cronenberg",
+  "Disease",
+];
+
+export const genderData = ["Male", "Female", "unknown", "Genderless"];
+
+export const statusData = ["Alive", "Dead", "unknown"];
---
+++ src/utils/query.js
@@ -0,0 +1,39 @@
+import { gql } from "@apollo/client";
+
+export const CHARACTERS = gql`
+  query (
+    $speciesC: String
+    $nameC: String
+    $genderC: String
+    $statusC: String
+    $pageNum: Int
+  ) {
+    characters(
+      filter: {
+        name: $nameC
+        species: $speciesC
+        gender: $genderC
+        status: $statusC
+      }
+      page: $pageNum
+    ) {
+      info {
+        count
+        pages
+      }
+      results {
+        name
+        status
+        image
+        species
+        gender
+        origin {
+          name
+        }
+        location {
+          name
+        }
+      }
+    }
+  }
+`;
