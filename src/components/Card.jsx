137b02c0bb5b97b1c2d41d432640583060125c54
MDeFaria <maria.defariasf@gmail.com>
2024-04-02 19:55:21 -0400

    add: card to home

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,10 @@
-export const Card = () => {
-  return <div>Card</div>;
+export const Card = ({ img, name }) => {
+  return (
+    <div className="relative max-w-[230px] max-h-[230px] overflow-hidden">
+      <img src={img} alt="" width={230} height={230} className="rounded-md" />
+      <div className="absolute bottom-0 text-center w-full rounded-md bg-accent/80 mx-auto">
+        <h2 className="text-xl py-1 rounded text-center font-medium">{name}</h2>
+      </div>
+    </div>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,18 @@
-export const Home = () => {
-  return <div>Home</div>;
+import { Card } from "../components";
+
+export const Home = ({ data, setPageNum, pageNum, loading }) => {
+  const { results } = data.characters;
+
+  return (
+    <div className="container mx-auto px-6 py-4">
+      {/* cards */}
+      <div
+        className={`grid justify-center grid-cols-5 gap-4
+      ${results.length <= 10 ? "grid-rows-2" : results.length <= 15 ? "grid-rows-3" : "grid-rows-4"}`}>
+        {results?.map(({ image, name }, index) => (
+          <Card key={index} img={image} name={name} />
+        ))}
+      </div>
+    </div>
+  );
 };
