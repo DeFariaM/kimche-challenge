f70c34411a73b3e967c5c4e911cf4b741a33009f
MDeFaria <maria.defariasf@gmail.com>
2024-04-02 23:23:21 -0400

    add: footer with socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,25 @@
+import { FaGithub, FaLinkedin, FaSuitcase } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="container mx-auto mb-6 flex justify-between px-6">
+      <div>
+        <Link target="_blank" to={"https://defariadev.vercel.app/es"}>
+          <img src="/brand.svg" width={300} />
+        </Link>
+      </div>
+      <div className="flex space-x-6 text-3xl text-accent">
+        <Link target="_blank" to={"https://github.com/DeFariaM"}>
+          <FaGithub />
+        </Link>
+        <Link target="_blank" to={"https://www.linkedin.com/in/mariadefaria/"}>
+          <FaLinkedin />
+        </Link>
+        <Link target="_blank" to={"https://defariadev.vercel.app/es"}>
+          <FaSuitcase />
+        </Link>
+      </div>
+    </div>
+  );
 };
