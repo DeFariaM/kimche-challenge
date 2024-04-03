2002836f90ac9181c78c64efed36f5f0c2a5f6e3
MDeFaria <maria.defariasf@gmail.com>
2024-04-02 23:23:34 -0400

    fix: loading spinner

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+export const Loading = () => {
+  return (
+    <div className="h-[80vh] items-center ">
+      <div className="mx-auto mt-16 w-fit">
+        <img src="/spinner.svg" width={400} />
+      </div>
+    </div>
+  );
+};
