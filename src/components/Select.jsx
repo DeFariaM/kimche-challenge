66d070a4c2af98b4989c934cab3fc6b448301084
MDeFaria <maria.defariasf@gmail.com>
2024-04-02 22:10:54 -0400

    fix: id of selects

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -2,14 +2,15 @@ export const Select = ({ name, data, setFunc }) => {
   return (
     <select
       name={name}
-      id={name.toLowerCase()}
+      id={name == "Gender" ? "gender" : name == "Status" ? "status" : "specie"}
       defaultValue={"placeholder"}
-      onChange={(e) => setFunc(e.target.value)}>
+      onChange={(e) => setFunc(e.target.value)}
+    >
       <option value="placeholder" disabled>
         {name}
       </option>
       {data?.map((item, index) => (
-        <option key={index} value={name}>
+        <option key={index} value={item}>
           {item}
         </option>
       ))}
